393b405d7ae453f88d46a5320851dd51469665be
linnea kleibrant <143947255+kleibrant@users.noreply.github.com>
2023-09-20 14:47:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,18 +3,13 @@ using System.Runtime.InteropServices;
 
 namespace MyConvini
 {
-
     class Program
     {
-       
-
         static void Main(string[] args)
         {
             Motor motor = new Motor();
 
             motor.Run();
         }
-
-        
     }
 }

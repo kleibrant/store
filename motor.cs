c4045b5a907d461774ec7d3490df4a944eee170f
linnea kleibrant <143947255+kleibrant@users.noreply.github.com>
2023-09-20 14:45:07 +0200

    Update motor.cs

--- motor.cs
+++ motor.cs
@@ -11,91 +11,88 @@ namespace MyConvini
         bool redo = true;
 
         public void Run() {
-        Store store = new Store();
-        ShoppingCart userCart = new ShoppingCart();
-
-       
-
-        while (redo == true)
-        {
-
-            Console.WriteLine("welcome to convini, how may we help you?");
-            Console.WriteLine("1. list available items");
-            Console.WriteLine("2. add an item to the shopping cart");
-            Console.WriteLine("3. view shopping cart");
-            Console.WriteLine("4. checkout");
-            Console.WriteLine("5. exit");
-
-            string choice = Console.ReadLine();
-
-            switch (choice)
+            Store store = new Store();
+            ShoppingCart userCart = new ShoppingCart();
+            while (redo == true)
             {
-                case "1":
-                    store.ListAvailableItems();
-                    Console.WriteLine("would you like to purchase one of these items? y/n");
 
-                    if (Console.ReadLine() == "y")
-                    {
-                        redo = true;
-                    }
-                    else
-                    {
-                        redo = false;
-                    }
-                    break;
+                Console.WriteLine("welcome to convini, how may we help you?");
+                Console.WriteLine("1. list available items");
+                Console.WriteLine("2. add an item to the shopping cart");
+                Console.WriteLine("3. view shopping cart");
+                Console.WriteLine("4. checkout");
+                Console.WriteLine("5. exit");
 
-                case "2":
-                    Console.WriteLine("what would you like to buy?");
-                        string itemName = Console.ReadLine();
-                        Item selectedItem = store.GetItemByName(itemName);
-                        userCart.AddToCart(selectedItem);
-                    Console.WriteLine("proceed? y/n");
+                string choice = Console.ReadLine();
 
-                    if (Console.ReadLine() == "y")
-                    {
-                        redo = true;
-                    }
-
-                    else if (Console.ReadLine() == "n")
-                    {
-                        redo = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("you didn't answer the question!! please say sorry (:");
-                        Console.ReadLine();
-                        if (Console.ReadLine() == "sorry")
+                switch (choice)
+                {
+                    case "1":
+                        store.ListAvailableItems();
+                        Console.WriteLine("would you like to purchase one of these items? y/n");
+    
+                        if (Console.ReadLine() == "y")
                         {
-                            Console.WriteLine("i forgive you, try again <3");
                             redo = true;
                         }
-                    }
-                    break;
-                case "3":
-                    Console.WriteLine("shopping cart:");
-                    userCart.ViewCart();
-                    Console.WriteLine("proceed? y/n");
+                        else
+                        {
+                            redo = false;
+                        }
+                        break;
 
-                    if (Console.ReadLine() == "y")
-                    {
-                        redo = true;
-                    }
-                    else
-                    {
-                        redo = false;
-                    }
-                    break;
-                case "4":
-                    store.Checkout(userCart);
-                    break;
-                case "5":
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("invalid option. please try again!");
-                    break;
+                    case "2":
+                        Console.WriteLine("what would you like to buy?");
+                            string itemName = Console.ReadLine();
+                            Item selectedItem = store.GetItemByName(itemName);
+                            userCart.AddToCart(selectedItem);
+                        Console.WriteLine("proceed? y/n");
+    
+                        if (Console.ReadLine() == "y")
+                        {
+                            redo = true;
+                        }
+    
+                        else if (Console.ReadLine() == "n")
+                        {
+                            redo = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("you didn't answer the question!! please say sorry (:");
+                            Console.ReadLine();
+                            if (Console.ReadLine() == "sorry")
+                            {
+                                Console.WriteLine("i forgive you, try again <3");
+                                redo = true;
+                            }
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("shopping cart:");
+                        userCart.ViewCart();
+                        Console.WriteLine("proceed? y/n");
+    
+                        if (Console.ReadLine() == "y")
+                        {
+                            redo = true;
+                        }
+                        else
+                        {
+                            redo = false;
+                        }
+                        break;
+                    case "4":
+                        store.Checkout(userCart);
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("invalid option. please try again!");
+                        break;
+                }
             }
         }
-    }
     }
 }
